a51307ae4d3b464242e01c5b793dc5e0daa5e294
Delong90 <matushkin030790@gmail.com>
2021-06-15 21:49:38 +0300

    init

--- app/src/main/java/com/example/testcustomview/MainActivity.kt
+++ app/src/main/java/com/example/testcustomview/MainActivity.kt
@@ -2,12 +2,9 @@ package com.example.testcustomview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
-import androidx.fragment.app.Fragment
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
